7bc4eeb4cc168d31774c3e1d23fb4a44ed5be5fc
wwwbrunagon <wwwbrunagon@gmail.com>
2021-06-28 10:09:20 -0300

    Structured configuration + implementation of Child Component

---
+++ src/components/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { User } from '../model/Model'
+import { AuthService } from '../services/AuthService'
+import { Login } from './Login'
+
+interface AppState {
+  user: User | undefined
+}
+
+export class App extends React.Component<{}, AppState>{
+
+  private authService: AuthService = new AuthService();
+
+  render() {
+    return (
+      <div>
+        Hello World
+        <Login />
+      </div>
+    )
+  }
+}
+
---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export class Login extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Login working
+            </div>
+        )
+    }
+}
